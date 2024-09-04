f98a898a577ccb2d66bde7ff3ccf0054be4e5c44
Mariia Fil <mariafil2107@gmail.com>
2024-09-04 17:27:07 +0400

    created assistant create endpoint

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,19 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/',
+    '/api/assistant/create'
+])
+
+export default clerkMiddleware((auth, request) => {
+    console.log('request')
+  if (!isPublicRoute(request)) {
+    console.log('hello')
+    auth().protect()
+  }
+})
 
 export const config = {
   matcher: [
